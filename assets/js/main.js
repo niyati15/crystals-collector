38477b22e7126b0ea751745b3251ec225f7ce3e7
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-05-01 00:03:57 -0700

    more css added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,99 @@
-
 $(document).ready(function () {
-    var crystals = ["crystal-1","crystal-2","crystal-3","crystal-4"];
-    var crystalVal = [9,7,6,1];
-    var randomNumber = Math.floor(Math.random() * 45) + 10;
-    $("#random-number").html(randomNumber);
-    console.log(randomNumber);
 
-    $("img").click(function () {
-        console.log("crystal clicked");
-        console.log(this.id);
-        scoreKeeper(this.id);
+    var wins = [];
+    var losses = [];
+    var totalScore = [];
 
-        function scoreKeeper(id) {
-            console.log("from function:", id);
+    $("#win-score").html(wins);
+    $("#loss-score").html(losses);
+    $("#your-score").html(totalScore);
+
+
+    function beginGame() {
+        var randomNumber = 0;
+        var max = 100;
+        var min = 30;
+        var randomCompNumber = Math.floor(Math.random() * max) + min;
+        randomNumber = randomCompNumber;
+
+        $("#target-score").html(randomCompNumber);
+        totalScore = 0;
+        $("#your-score").html(totalScore);
+
+        var crystal1 = 0;
+        var crystalTotalValue = Math.floor(Math.random() * 11) + 1;
+        var crystal1 = crystalTotalValue;
+        $(document).on("click", "#crystal-1", function () {
+            totalScore += crystal1;
+            $("#your-score").html(totalScore);
+            checkScore();
+        });
+
+
+        var crystal2 = 0;
+        var crystalTotalValue = Math.floor(Math.random() * 11) + 1;
+        var crystal2 = crystalTotalValue;
+        $(document).on("click", "#crystal-2", function () {
+            totalScore += crystal2;
+            $("#your-score").html(totalScore);
+            checkScore();
+        });
+
+
+        var crystal3 = 0;
+        var crystalTotalValue = Math.floor(Math.random() * 11) + 1;
+        var crystal3 = crystalTotalValue;
+        $(document).on("click", "#crystal-4", function () {
+            totalScore += crystal3;
+            $("#your-score").html(totalScore);
+            checkScore();
+        });
+
+
+        var crystal4 = 0;
+        var crystalTotalValue = Math.floor(Math.random() * 11) + 1;
+        var crystal4 = crystalTotalValue;
+        $(document).on("click", "#crystal-5", function () {
+            totalScore += crystal4;
+            $("#your-score").html(totalScore);
+            checkScore();
+        });
+
+
+        function checkScore() {
+            if (totalScore === randomNumber) {
+                wins++;
+                $("#win-score").html(wins);
+                totalScore = 0;
+                $("#your-score").html(0);
+                resetGame();
+            }
+            else if (totalScore > randomNumber) {
+                losses++;
+                $("#loss-score").html(losses);
+                resetGame();
+            }
+        }
+
+        function resetGame() {
+            totalScore = 0;
+            crystal1 = 0;
+            crystal2 = 0;
+            crystal3 = 0;
+            crystal4 = 0;
+            var max = 100;
+            var min = 30;
+            var randomCompNumber = Math.floor(Math.random() * max) + min;
+            randomNumber = randomCompNumber;
+            $("#target-score").html(randomNumber);
+            $("#your-score").html(totalScore)
+            beginGame();
 
         }
-    });
+
+    }
+
+    beginGame();
+
 });
+
